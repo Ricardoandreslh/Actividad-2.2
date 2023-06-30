810f56fa009210282a43c1b19acd5e48d26b4f48
Ricardoandreslh <ricardoandresloaiza@gmail.com>
2023-06-29 21:42:53 -0400

    Commit con animaciones, menu nuevo y menu hamburguesa que falta pulir todavia

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
 import './style.css'
+
+window.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('.navbar').classList.add('animate');
+    document.querySelector('.menu').classList.add('animate');
+    var dropdowns = document.querySelectorAll('.dropdown-menu');
+    dropdowns.forEach(function(dropdown) {
+      dropdown.classList.add('animate');
+    });
+  });
